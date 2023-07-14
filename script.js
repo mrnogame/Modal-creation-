a6ac2c41c8330859fd60f0e894aeea69249744c8
adam <adam.00228@hotmail.fr>
2023-07-14 13:24:25 +0200

    commit

---
+++ script.js
@@ -0,0 +1,36 @@
+var openModalBtn = document.querySelectorAll('[data-modal-target]')
+var closeModalBtn = document.querySelectorAll('[data-close-button]')
+var overlay =document.getElementById('overlay')
+
+openModalBtn.forEach(button => {
+    button.addEventListener('click',()=>{
+        const modal = document.querySelector(button.dataset.modalTarget)
+        openModal(modal)
+    } );
+});
+
+overlay.addEventListener('click', () => {
+    const modals = document.querySelectorAll('.modal.active')
+    modals.forEach(modal => {
+      closeModal(modal)
+    });
+  });
+closeModalBtn.forEach(button => {
+    button.addEventListener('click',()=>{
+        const modal = button.closest('.modal')
+        closeModal(modal)
+    } );   
+});
+
+function openModal(modal){
+    if (modal ==null ) return
+    modal.classList.add('active')
+    overlay.classList.add('active')
+
+}
+
+function closeModal(modal){
+    if (modal == null) return
+    modal.classList.remove('active')
+    overlay.classList.remove ('active')
+}
